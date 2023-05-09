cabc56874ea9693043d8197797a5831eb83b7822
Emre Güçlü <emreg@microsoft.com>
2023-05-09 13:14:45 +0300

    added a file

--- util/Scripts/CreateGitHubSecrets.ps1
+++ util/Scripts/CreateGitHubSecrets.ps1
@@ -11,14 +11,19 @@ param (
 Connect-AzAccount -TenantId $tenantId -SubscriptionId $subscriptionId
 
 # Create a Service Principal
-$spName = "GitHub-Actions-SPN"
+$spName = "$githubOwner-GitHub-Actions-SPN"
 $sp = New-AzADServicePrincipal -DisplayName $spName -SkipAssignment
+
+if ($null -eq $sp -or $null -eq $sp.Id) {
+    throw "Failed to create the Service Principal. Please check the Azure account connection and try again."
+}
+
 
 # Retrieve the resource group
 $resourceGroup = Get-AzResourceGroup -Name $resourceGroupName
 
 # Assign the "Contributor" role to the Service Principal on the resource group
-New-AzRoleAssignment -ObjectId $sp.ObjectId -RoleDefinitionName "Contributor" -ResourceGroupName $resourceGroupName -Scope $resourceGroup.ResourceId
+New-AzRoleAssignment -ApplicationId $sp.ApplicationId -RoleDefinitionName "Contributor" -ResourceGroupName $resourceGroup.ResourceGroupName
 
 # Retrieve the required values
 $azureClientId = $sp.ApplicationId
@@ -37,13 +42,22 @@ $githubApiUrl = "https://api.github.com/repos/$githubOwner/$githubRepo/actions/s
 function Add-GitHubSecret($name, $value) {
     $body = @{
         encrypted_value = (ConvertTo-SecureString -String $value -AsPlainText -Force | ConvertFrom-SecureString)
-        key_id          = (Invoke-RestMethod -Uri "$githubApiUrl/public-key" -Headers $headers).key_id
+        key_id          = (Invoke-RestMethod -Uri "$($script:githubApiUrl)/public-key" -Headers $headers).key_id
     } | ConvertTo-Json
 
-    Invoke-RestMethod -Method Put -Uri "$githubApiUrl/$name" -Headers $headers -Body $body -ContentType "application/json"
+    Invoke-RestMethod -Method Put -Uri "$($script:githubApiUrl)/$name" -Headers $headers -Body $body -ContentType "application/json"
+}
+$githubVariablesApiUrl = "https://api.github.com/repos/$githubOwner/$githubRepo/actions/variables"
+function Add-GitHubVariable($name, $value) {
+    $body = @{
+        encrypted_value = (ConvertTo-SecureString -String $value -AsPlainText -Force | ConvertFrom-SecureString)
+        key_id          = (Invoke-RestMethod -Uri "$($script:githubVariablesApiUrl)/public-key" -Headers $headers).key_id
+    } | ConvertTo-Json
+
+    Invoke-RestMethod -Method Put -Uri "$($script:githubVariablesApiUrl)/$name" -Headers $headers -Body $body -ContentType "application/json"
 }
 
-Add-GitHubSecret "AZURE_CLIENT_ID" $azureClientId
+Add-GitHubVariable "AZURE_CLIENT_ID" $azureClientId -githubApiUrl 
 Add-GitHubSecret "AZURE_CLIENT_SECRET" $azureClientSecret
-Add-GitHubSecret "AZURE_TENANT_ID" $azureTenantId
-Add-GitHubSecret "AZURE_SUBSCRIPTION_ID" $azureSubscriptionId
+Add-GitHubVariable "AZURE_TENANT_ID" $azureTenantId
+Add-GitHubVariable "AZURE_SUBSCRIPTION_ID" $azureSubscriptionId
