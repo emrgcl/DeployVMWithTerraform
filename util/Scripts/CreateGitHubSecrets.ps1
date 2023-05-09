1a09239ec973e9f7259b246c223b487b45f91b07
Emre Güçlü <emreg@microsoft.com>
2023-05-09 19:13:35 +0300

    updated config

--- util/Scripts/CreateGitHubSecrets.ps1
+++ util/Scripts/CreateGitHubSecrets.ps1
@@ -27,7 +27,7 @@ New-AzRoleAssignment -ApplicationId $sp.ApplicationId -RoleDefinitionName "Contr
 
 # Retrieve the required values
 $azureClientId = $sp.ApplicationId
-$azureClientSecret = ($sp.Secret | ConvertTo-SecureString -AsPlainText -Force)
+$azureClientSecret = ($sp.Secret | Convertfrom-SecureString -AsPlainText )
 $azureTenantId = $tenantId
 $azureSubscriptionId = $subscriptionId
 
@@ -50,14 +50,30 @@ function Add-GitHubSecret($name, $value) {
 $githubVariablesApiUrl = "https://api.github.com/repos/$githubOwner/$githubRepo/actions/variables"
 function Add-GitHubVariable($name, $value) {
     $body = @{
-        encrypted_value = (ConvertTo-SecureString -String $value -AsPlainText -Force | ConvertFrom-SecureString)
-        key_id          = (Invoke-RestMethod -Uri "$($script:githubVariablesApiUrl)/public-key" -Headers $headers).key_id
-    } | ConvertTo-Json
-
-    Invoke-RestMethod -Method Put -Uri "$($script:githubVariablesApiUrl)/$name" -Headers $headers -Body $body -ContentType "application/json"
+        name = $Name
+        value = $Value
+      } | ConvertTo-Json
+   $Headers = @{
+    Authorization = "Bearer $script:githubToken"
+   }
+    Invoke-RestMethod -Method Post -Uri "$($script:githubVariablesApiUrl)" -Headers $headers -Body $body -ContentType "application/json"
 }
 
-Add-GitHubVariable "AZURE_CLIENT_ID" $azureClientId -githubApiUrl 
-Add-GitHubSecret "AZURE_CLIENT_SECRET" $azureClientSecret
-Add-GitHubVariable "AZURE_TENANT_ID" $azureTenantId
-Add-GitHubVariable "AZURE_SUBSCRIPTION_ID" $azureSubscriptionId
+try {
+Add-GitHubVariable "AZURE_CLIENT_ID" $azureClientId -githubApiUrl -erroraction stop 
+Add-GitHubSecret "AZURE_CLIENT_SECRET" $azureClientSecret -erroraction stop
+Add-GitHubSecret "GH_TOKEN" $githubToken -erroraction stop
+Add-GitHubVariable "AZURE_TENANT_ID" $azureTenantId -erroraction stop   
+Add-GitHubVariable "AZURE_SUBSCRIPTION_ID" $azureSubscriptionId -erroraction stop
+write-verbose "Successfully created the GitHub secrets and variables."
+Write-Output "Azure_Client_Id: $azureClientId"
+Write-output "Azure_Client_Secret: $azureClientSecret"
+Write-output "NOTE: Please note Azure_client_Secret, as it will be only visible this time."
+write-output "Azure_Tenant_Id: $azureTenantId"
+write-output "Azure_Subscription_Id: $azureSubscriptionId"
+write-output "GitHub_Token: $githubToken"
+
+}
+catch {
+    throw "Failed to create the GitHub secrets. Please check the GitHub token and try again."
+}
